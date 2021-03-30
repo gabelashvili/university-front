5899636c33adb7378ecd8feb5a9573056b4304bb
gabelashvili <gabelashvili1999@gmail.com>
2021-03-30 22:17:41 +0400

    add

--- src/components/Auth/index.js
+++ src/components/Auth/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 const Auth = () => (
   <div>
-    Auth
+    <input type="text" name="username" placeholder="username" />
+    <input type="password" name="password" placeholder="password" />
   </div>
 );
 
