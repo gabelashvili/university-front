6e8dd84e301508f095adbc721a75d12915acd5aa
gabelashvili <gabelashvili1999@gmail.com>
2021-04-01 20:07:33 +0400

    refactor

--- src/helpers/routes.js
+++ src/helpers/routes.js
@@ -1,18 +1,6 @@
-import BaseLayout from 'pages/BaseLayout';
-import Register from 'components/Register';
 import Auth from 'components/Auth';
 
 export const routes = [
-  {
-    name: 'Home',
-    component: BaseLayout,
-    path: '/',
-  },
-  {
-    name: 'Register',
-    component: Register,
-    path: '/register',
-  },
   {
     name: 'Login',
     component: Auth,
