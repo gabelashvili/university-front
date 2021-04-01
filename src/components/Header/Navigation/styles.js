34276377afc28068312218c193cd80b0dd715e69
gabelashvili <gabelashvili1999@gmail.com>
2021-04-01 22:49:00 +0400

    ref

--- src/components/Header/Navigation/styles.js
+++ src/components/Header/Navigation/styles.js
@@ -13,9 +13,9 @@ const navigationAnimation = keyframes`
 export const Nav = styled.nav`
     width: 100%;
     height: 70px;
-    background-color: ${(props) => (props.isScrolled && 'white')};
-    position: ${(props) => (props.isScrolled && 'fixed')};
-    animation: ${(props) => (props.isScrolled === true ? css`${navigationAnimation} 0.5s` : '')};
+    background-color: ${({ isScrolled, theme }) => (isScrolled ? theme.colors.white : 'transparent')};
+    position: ${({ isScrolled }) => (isScrolled ? 'fixed' : 'relative')};
+    animation: ${({ isScrolled }) => (isScrolled === true ? css`${navigationAnimation} 0.5s` : '')};
     border-bottom: 1px solid #e0e0e0;
     z-index: 1;
     box-shadow: 0 0 12px 0 rgb(0 0 0 / 12%);
--- src/index.js
+++ src/index.js
@@ -4,13 +4,17 @@ import './index.css';
 import App from 'components/root/App';
 import Store from 'helpers/store';
 import { Provider } from 'react-redux';
+import { ThemeProvider } from 'styled-components';
+import themes from 'helpers/themes';
 
 const store = Store;
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ThemeProvider theme={themes}>
+        <App />
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
