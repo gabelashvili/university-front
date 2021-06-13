2a3d20451fcdedd853b06b182e3c50334d7ae470
gabelashvili <gabelashvili1999@gmail.com>
2021-06-13 19:31:29 +0400

    fx

--- src/components/University/Feed/Comments/index.js
+++ src/components/University/Feed/Comments/index.js
@@ -127,7 +127,6 @@ const Comments = ({
                   Reply
                   {' '}
                   (
-                  {' '}
                   {comment.replyCnt}
                   )
                 </Button>
