d3b116a93f37abe166dc6186949503eddc69238c
gabelashvili <gabelashvili1999@gmail.com>
2021-06-21 18:06:08 +0400

    fx

--- src/components/University/Lectures/index.js
+++ src/components/University/Lectures/index.js
@@ -1,7 +1,57 @@
-const Lectures = () => (
-  <div>
-    lectures
-  </div>
+import {
+  containerStyles,
+  Lectures,
+  Lecture,
+  LectureImg,
+  LectureTitle,
+  LectureDesc,
+  RaitingWrapper,
+  buttonStyles,
+  ButtonWrapper,
+  Filters,
+  FilterTitle,
+  Search,
+  Ul,
+  Li,
+} from 'components/University/Lectures/styles';
+import Container from 'components/Container';
+import Raiting from 'components/Raiting';
+import Button from 'components/Button';
+
+const LecturesComponent = () => (
+  <Container costumStyles={containerStyles}>
+    <Filters>
+      <FilterTitle>ფილტრი</FilterTitle>
+      <Search type="text" placeholder="ლექტორის სახელი" />
+      <Ul>
+        <Li>ინფორმატიკა</Li>
+        <Li>ინფორმატიკა</Li>
+        <Li>ინფორმატიკა</Li>
+        <Li>ინფორმატიკა</Li>
+        <Li>ინფორმატიკა</Li>
+        <Li>ინფორმატიკა</Li>
+      </Ul>
+    </Filters>
+    <Lectures>
+      {new Array(10).fill().map(() => (
+        <Lecture>
+          <LectureImg src="https://deadline.com/wp-content/uploads/2020/03/jimhouston.jpg?w=360&h=383&crop=1" />
+          <LectureTitle>მაქსიმ იავიჩი</LectureTitle>
+          <LectureDesc>
+            მოწვეული ლექტორი
+            <br />
+            ინფორმატიკა
+          </LectureDesc>
+          <RaitingWrapper>
+            <Raiting isDisabled={false} />
+          </RaitingWrapper>
+          <ButtonWrapper>
+            <Button costumStyles={buttonStyles}>შეფასებები</Button>
+          </ButtonWrapper>
+        </Lecture>
+      ))}
+    </Lectures>
+  </Container>
 );
 
-export default Lectures;
+export default LecturesComponent;
---
+++ src/components/University/Lectures/styles.js
@@ -0,0 +1,121 @@
+import { transparentize } from 'polished';
+import styled, { css } from 'styled-components';
+
+export const containerStyles = css`
+    padding: 10px;
+    border-radius: 5px;
+    padding-bottom: 150px;
+    display: grid;
+    grid-template-columns: 1fr 3fr;
+    grid-gap: 50px;
+`;
+
+export const Lectures = styled.div`
+    display: grid;
+    grid-gap: 20px;
+    grid-template-columns: repeat(3, 1fr);
+`;
+
+export const ButtonWrapper = styled.div`
+    transform: scaleY(0) translateY(100%);
+    transition: all 0.3s;
+`;
+
+export const buttonStyles = css`
+    border: 1px solid ${({ theme }) => transparentize(0.1, theme.colors.blue)};
+    color: ${({ theme }) => theme.colors.black};
+    border-radius: 5px;
+    padding: 8px;
+    cursor: pointer;
+    &:hover {
+        background-color: ${({ theme }) => transparentize(0.9, theme.colors.blue)};
+    }
+`;
+
+export const Lecture = styled.div`
+    padding: 20px 20px 30px;
+    text-align: center;
+    box-shadow: 0px 0px 18px 1px rgb(0 0 0 / 5%);
+    width: 100%;
+    height: max-content;
+    border-bottom: 1px solid ${({ theme }) => transparentize(0.1, theme.colors.black)};
+    background-color: ${({ theme }) => theme.colors.white};
+    cursor: pointer;
+    position: relative;
+    transition: all 0.5s;
+    &:hover {
+        padding-bottom: 68px;
+        margin-bottom: -75px;
+        z-index: 1;
+        & ${ButtonWrapper} {
+            transform: scaleY(1) translateY(100%);
+        }
+    }
+`;
+
+export const LectureImg = styled.img`
+    border-radius: 50%;
+    border: 10px solid ${({ theme }) => transparentize(0.9, theme.colors.blue)};
+    width: 120px;
+    height: 120px;
+`;
+
+export const LectureTitle = styled.h2`
+    font-weight: 700;
+    font-size: 22px;
+    margin: 20px 0 5px;
+`;
+
+export const LectureDesc = styled.p`
+    font-size: 13px;
+    color:${({ theme }) => transparentize(0.2, theme.colors.lightGrey)};
+    font-weight: 500;
+    margin-bottom: 20px;
+`;
+
+export const RaitingWrapper = styled.div`
+    display: flex;
+    justify-content: center;
+`;
+
+export const Filters = styled.div`
+    background: red;
+    height: max-content;
+    box-shadow: 3px 3px 9px rgb(0 0 0 / 8%);
+    background-color: ${({ theme }) => transparentize(0.1, theme.colors.white)};;
+    padding: 10px 20px;
+`;
+
+export const FilterTitle = styled.h1`
+    font-weight: 600;
+    font-size: 18px;
+    border-bottom: 1px solid ${({ theme }) => transparentize(0.7, theme.colors.black)};
+    padding-bottom: 10px;
+`;
+
+export const Search = styled.input`
+    width: 100%;
+    border: 1px solid ${({ theme }) => transparentize(0.7, theme.colors.black)};
+    padding: 8px;
+    color: ${({ theme }) => transparentize(0.3, theme.colors.lightGrey)};
+    &:focus {
+        outline: 0;
+    };
+    margin: 30px 0 20px;
+`;
+
+export const Ul = styled.ul`
+    padding-left: 20px;
+`;
+
+export const Li = styled.li`
+    cursor: pointer;
+    font-size: 14px;
+    font-weight: 500;
+    color: ${({ theme }) => transparentize(0.3, theme.colors.lightGrey)};
+    margin-bottom: 7px;
+    transition: color 0.3s;
+    &:hover {
+        color: ${({ theme }) => transparentize(0.3, theme.colors.lightGreen)};
+    }
+`;
